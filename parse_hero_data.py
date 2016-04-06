07663f4845cc28c98f30241ffac4d2fa14460a4f
albog <quietkane@gmail.com>
2016-04-06 16:56:04 -0500

    Small progress.

--- parse_hero_data.py
+++ parse_hero_data.py
@@ -1,16 +1,43 @@
-with open('hero_data.txt', 'rb') as file_in:
+from pickle import load, dump
+
+HERO_NAMES = ['Leonhardt', 'Ladius', 'Thoma', 'Duran', 'Rex']
+with open('hero_data.txt', 'r') as file_in:
     hero_data = [line.strip() for line in file_in if line.strip() != '']
-gen = range(6)
+gen = [i for i in range(7)]
 for i in range(len(hero_data)):
     for j in range(1, 6):
         if hero_data[i].endswith(str(j).join(['<GEN', '>'])):
             gen[j] = i
     if hero_data[i].endswith('<WPR>'):
         gen[6] = i
-leon = hero_data[gen[1]:gen[2]]
-ladius = hero_data[gen[2]:gen[3]]
-thoma = hero_data[gen[3]:gen[4]]
-duran = hero_data[gen[4]:gen[5]]
-rex = hero_data[gen[5]:gen[6]]
+hero_strings = [hero_data[gen[i]:gen[i+1]] for i in range(1, 6)]
+for h in hero_strings:
+    for i in range(len(h)):
+        line = [j for j in h[i].split(' ') if j != '']
+        h[i] = ' '.join(line)
+hero_data = {HERO_NAMES[i]: hero_strings[i] for i in range(5)}
+for name in hero_data:
+    hero_data[name] = [line for line in hero_data[name] if not line.startswith(name)]
+    breaks = [0]
+    breaks.extend([hero_data[name].index(line) for line in hero_data[name] if line.endswith('>')])
+    breaks.append(-1)
+    hero_data[name] = [hero_data[name][breaks[i]:breaks[i+1]] for i in range(len(breaks))
+                       if i != len(breaks)-1 and
+                       hero_data[name][breaks[i]:breaks[i+1]]]
+    l = []
+    for lineage in hero_data[name]:
+        stats = {line.split(' ')[0]: line.split(' ')[1:] for line in lineage}
+        for st in stats:
+            split = stats[st][0].split('-')
+            if len(split) > 1:
+                stats[st][0] = split[0]
+                stats[st].insert(1, split[1])
+        l.append(stats)
+    hero_data[name] = l
 
-print('\n'.join(leon))
+
+with open('proficiency_costs', 'rb') as file_in:
+    prof_costs = load(file_in)
+
+print(len(hero_data[HERO_NAMES[2]]))
+
